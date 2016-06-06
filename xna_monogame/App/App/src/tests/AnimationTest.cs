e7da309a810f52d8d90df874edb0693d076ef721
Maximilian Li <maximilian.xiling.li@gmail.com>
2016-06-06 17:24:22 +0200

    xna testclasses added

---
+++ xna_monogame/App/App/src/tests/AnimationTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace App.src.tests
+{
+    class AnimationTest : ITest
+    {
+        //Bunnies
+        private List<Bunny> bunnies;
+        private int bunnyCount = 0;
+
+        private int step;
+
+        private Random random;
+        private ContentManager content;
+        SpriteBatch spriteBatch;
+
+        //Animation Constants
+        private float gravity = 0.5f;
+        private float maxX;
+        private float minX;
+        private float maxY;
+        private float minY;
+
+        public AnimationTest(ContentManager content, SpriteBatch spriteBatch)
+        {
+            this.content = content;
+            this.spriteBatch = spriteBatch;
+        }
+
+
+        public int RunTest(GameTime gameTime)
+        {
+            AddBunnies(step);
+
+            //bunnies movement
+            for (int i = 0; i < bunnies.Count; i++)
+            {
+                Bunny bunny = bunnies[i];
+                bunny.jump(random, gravity, minX, minY, maxX, maxY);
+            }
+            return bunnyCount;
+        }
+
+        public void Initialize(int min_val, float maxX, float maxY, int step)
+        {
+            bunnies = new List<Bunny>();
+            random = new Random();
+
+            this.minX = 0;
+            this.minY = 0;
+            this.maxX = maxX;
+            this.maxY = maxY;
+            this.step = step;
+
+            AddBunnies(min_val);
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            for (int i = 0; i < bunnies.Count; i++)
+            {
+                Bunny bunny = bunnies[i];
+                spriteBatch.Draw(bunny.texture, new Vector2(bunny.X, bunny.Y), null, Color.White);
+            }
+
+        }
+
+        /// <summary>
+        /// Add Bunnies to the Scenery
+        /// </summary>
+        /// <param name="count"> The Amount of Bunnies to be added</param>
+        public void AddBunnies(int count = 100)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Bunny bunny = new Bunny(content);
+
+                bunnies.Add(bunny);
+            }
+            bunnyCount += count;
+        }
+    }
+}
---
+++ xna_monogame/App/App/src/tests/ITest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace App.src.tests
+{
+    interface ITest
+    {
+        int RunTest(GameTime gameTime);
+        void Initialize(int min_val, float maxX, float maxY, int step);
+        void Draw(GameTime gameTime);
+    }
+}
