ecc38e93fedc35a4587aec37106eba0af9e46e55
MelvilQ <patpp17@web.de>
2016-06-30 16:31:56 +0200

    LibGDX is startable by the manager script

--- test-manager.py
+++ test-manager.py
@@ -18,7 +18,10 @@ import matplotlib.pyplot as plt
 
 def run_test(frameworks, test_name, min_val, max_val, step):
 	for framework in frameworks:
-		subprocess.call([framework + "/bin/App.exe", test_name, str(min_val), str(max_val), str(step)], cwd=framework+"/bin")
+		if framework == "LibGDX":
+			subprocess.call(["java", "-jar", "App.jar", test_name, str(min_val), str(max_val), str(step)], cwd=framework+"/bin")
+		else:
+			subprocess.call([framework + "/bin/App.exe", test_name, str(min_val), str(max_val), str(step)], cwd=framework+"/bin")
 	make_diagram(frameworks, test_name)
 
 def make_diagram(frameworks, test_name):
@@ -44,11 +47,9 @@ def get_data_from_log(framework, test_name):
 
 ###################################################################################################		
 		
-frameworks = ['xna_monogame','sdl']
+frameworks = ['SDL','xna_monogame','LibGDX']
 
 # run some tests for all frameworks
-run_test(frameworks, "standard", 1000, 20000, 1000)
 run_test(frameworks, "random", 1000, 20000, 1000)
-run_test(frameworks, "scaled", 1000, 20000, 1000)
 
 ###################################################################################################
