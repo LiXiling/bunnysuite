e39ce45e022c19863073391c418a903bc39f68f5
Maximilian Li <maximilian.xiling.li@gmail.com>
2016-07-06 16:04:31 +0200

    xna testcombination parsing

--- xna_monogame/App/App/src/model/BenchmarkFactory.cs
+++ xna_monogame/App/App/src/model/BenchmarkFactory.cs
@@ -18,44 +18,48 @@ namespace App.src.model
         /// <param name="maxVal">End Value for the Test</param>
         /// <param name="step">Step increment for the Test</param>
         /// <returns></returns>
-        public BenchmarkTest ConstructBenchmark(String testname, int minVal, int maxVal, int step)
+        public BenchmarkTest ConstructBenchmark(String testnameList, int minVal, int maxVal, int step)
         {
             BenchmarkTest bt = new BenchmarkTest(minVal, maxVal, step);
+            String[] testnames = testnameList.split(",");
 
-            switch (testname)
-            {
-                case "standard":
-                    bt.addSpawnModifier(new FixedPositionModifier());
-                    break;
-                case "animation":
-                    bt.addSpawnModifier(new SpeedModifier());
-                    bt.addUpdateModifier(new AnimationModifier());
-                    break;
-                case "texturechange":
-                    bt.addSpawnModifier(new RandomPositionModifier());
-                    bt.addUpdateModifier(new TexturechangeModifier());
-                    break;
-                case "rotation":
-                    bt.addSpawnModifier(new RandomPositionModifier());
-                    bt.addUpdateModifier(new RotationModifier());
-                    break;
-                case "random":
-                    RandomPositionModifier rt = new RandomPositionModifier();
-                    bt.addSpawnModifier(rt);
-                    bt.addUpdateModifier(rt);
-                    break;
-                case "scaled":
-                    bt.addSpawnModifier(new RandomPositionModifier());
-                    bt.addUpdateModifier(new ScaleModifier());
-                    break;
-                case "multitexture":
-                    bt.addSpawnModifier(new RandomPositionModifier());
-                    bt.addSpawnModifier(new TexturechangeModifier());
-                    break;
-                default:
-                    bt.addSpawnModifier(new SpeedModifier());
-                    bt.addUpdateModifier(new AnimationModifier());
-                    break;
+            foreach(String testname : testnames){
+
+                switch (testname)
+                {
+                    case "standard":
+                        bt.addSpawnModifier(new FixedPositionModifier());
+                        break;
+                    case "animation":
+                        bt.addSpawnModifier(new SpeedModifier());
+                        bt.addUpdateModifier(new AnimationModifier());
+                        break;
+                    case "texturechange":
+                        bt.addSpawnModifier(new RandomPositionModifier());
+                        bt.addUpdateModifier(new TexturechangeModifier());
+                        break;
+                    case "rotation":
+                        bt.addSpawnModifier(new RandomPositionModifier());
+                        bt.addUpdateModifier(new RotationModifier());
+                        break;
+                    case "random":
+                        RandomPositionModifier rt = new RandomPositionModifier();
+                        bt.addSpawnModifier(rt);
+                        bt.addUpdateModifier(rt);
+                        break;
+                    case "scaled":
+                        bt.addSpawnModifier(new RandomPositionModifier());
+                        bt.addUpdateModifier(new ScaleModifier());
+                        break;
+                    case "multitexture":
+                        bt.addSpawnModifier(new RandomPositionModifier());
+                        bt.addSpawnModifier(new TexturechangeModifier());
+                        break;
+                    default:
+                        bt.addSpawnModifier(new SpeedModifier());
+                        bt.addUpdateModifier(new AnimationModifier());
+                        break;
+                    }
             }
 
             return bt;
