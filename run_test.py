3cbad43a8e86f28c775331635441692580a16202
MelvilQ <patpp17@web.de>
2016-08-24 15:58:46 +0200

    ideenliste

--- run_test.py
+++ run_test.py
@@ -1,8 +1,8 @@
 from testmanager import *
 
 frameworks = ['SDL']
-test_name = 'particles,animation'
-min_val = 100
+test_name = 'multitexture,alpha,thin,animation,texturechange'
+min_val = 2000
 max_val = 200000
-step = 100
+step = 20
 run_test(frameworks, test_name, min_val, max_val, step)
