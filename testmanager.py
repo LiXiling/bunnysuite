ec0f25bc3598716b3b3d19fa6d27d350883b50f2
Victor Schümmer <victor_ferdinand.schuemmer@stud.tu-darmstadt.de>
2016-09-06 18:12:35 +0200

    testmanager creates report file

--- testmanager.py
+++ testmanager.py
@@ -3,6 +3,7 @@
 import subprocess
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
 def run_test(frameworks, test_name, min_val, max_val, step, res_x=800, res_y=600, repetitions = 10):
     print (frameworks, test_name, min_val, max_val, step, res_x, res_y)
@@ -11,10 +12,12 @@ def run_test(frameworks, test_name, min_val, max_val, step, res_x=800, res_y=600
             subprocess.call([framework + "/bin/App.exe", test_name, str(min_val), str(max_val), str(step), str(res_x), str(res_y), str(repetitions)], cwd=framework+"/bin")
         except:
             subprocess.call(["java", "-jar", "App.jar", test_name, str(min_val), str(max_val), str(step), str(res_x), str(res_y), str(repetitions)], cwd=framework+"/bin")    
-    make_bar_diagram(frameworks, test_name)
-    make_detail_diagram(frameworks, test_name)
-    
-def make_bar_diagram(frameworks, test_name):
+    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H-%M-%S")
+    make_report_file(frameworks, test_name, timestamp, min_val, max_val, step, repetitions, res_x, res_y)
+    make_bar_diagram(frameworks, test_name, timestamp)
+    make_detail_diagram(frameworks, test_name, timestamp)
+     
+def make_bar_diagram(frameworks, test_name, name_prefix):
     thresholds = []
     for framework in frameworks:
         (x, y) = get_data_from_log(framework, test_name)
@@ -29,19 +32,19 @@ def make_bar_diagram(frameworks, test_name):
     plt.ylabel("# objects when framerate drops under 56 fps")
     plt.yticks(np.arange(0, max_threshold*1.5, max_threshold/6.0))
     plt.grid(True)
-    plt.savefig("results/" + test_name + "_thresholds.png")
+    plt.savefig("results/" + name_prefix + "_thresholds.png")
     plt.gcf().canvas.set_window_title("Threshold values of the test: " + test_name)
     plt.show()
         
 def get_threshold_value_from_data(x,y):
     if len(y) == 0:
         return 0
-    for i in reversed(range(0,len(y))):
+    for i in reversed(range(0, len(y))):
         if y[i] < 0.02:
             return x[i]
     return x[0]
         
-def make_detail_diagram(frameworks, test_name):
+def make_detail_diagram(frameworks, test_name, name_prefix):
     for framework in frameworks:
         (x, y) = get_data_from_log(framework, test_name)
         plt.plot(x, y, label=framework)
@@ -49,8 +52,8 @@ def make_detail_diagram(frameworks, test_name):
     plt.xlabel("#")
     plt.ylabel("Average rendering time in seconds")
     plt.grid(True)
-    plt.legend()
-    plt.savefig("results/" + test_name + "_detailed.png")
+    plt.legend(loc=2)
+    plt.savefig("results/" + name_prefix + "_detailed.png")
     plt.gcf().canvas.set_window_title("Detailed results of the test: " + test_name)
     plt.show()
     
@@ -65,3 +68,42 @@ def get_data_from_log(framework, test_name):
     except:
         print ("no log file found for " + framework)
         return ([],[])
+        
+def make_report_file(frameworks, test_name, name_prefix, minBunnies,
+                     maxBunnies, stepSize, repetitions, width, height):
+    outString = 'Bunnysuite report created at %s\n\n' % datetime.datetime.now().strftime("%Y/%m/%d %H:%M")
+    outString += 'modifiers: %s\n' % test_name.replace(',', ', ')
+    outString += 'frameworks: %s\nminBunnies: %s\nmaxBunnies: %s\nstepSize: %s\nrepetitions: %s\nresolution: %sx%s\n\n' % (', '.join(frameworks), minBunnies, maxBunnies, stepSize, repetitions, width, height)
+    
+    logs = {}
+    
+    for framework in frameworks:
+        f = open(framework + '/bin/log/' + test_name + '.log', 'r')
+        logs[framework] = f.read().splitlines()
+        f.close()
+    
+    outString += 'bunnies\t%s\n' % '\t'.join(frameworks)
+    
+    for i in range(len(logs[frameworks[0]])):
+        logline = logs[frameworks[0]][i].split('\t')[0]
+        for framework in frameworks:
+            logline += '\t' + logs[framework][i].split('\t')[1]
+        outString += logline + '\n'
+  
+    out = open("results/" + name_prefix + "_report.log", "w")
+    out.write(outString)
+    out.close()
+    
+def find_frameworks():
+    # Returns a list with all names of available frameworks by searching for
+    # subdirectories containing a 'bin' folder containing App.jar/App.exe.
+    import os
+    frameworks = []    
+    for directory in next(os.walk('.'))[1]:
+        if 'bin' in next(os.walk(directory))[1]:
+            for fname in os.listdir(directory+'/bin'):
+                if fname == 'App.exe' or fname == 'App.jar':
+                    frameworks.append(directory)
+                    break
+    return frameworks
+        
